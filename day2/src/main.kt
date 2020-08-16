8ccbcea18c6b34949d63a9bb39ab7a5bbf8f0be8
Your Name <you@example.com>
2020-08-16 12:30:41 +0700

    สร้าง override function ให้กับ HippoกับCat

--- day2/src/main.kt
+++ day2/src/main.kt
@@ -1,7 +1,18 @@
 fun main() {
-    var hip = Hippo ()
-    hip.eat()
+    var myhippo = Hippo()
+    myhippo.eat()
+    myhippo.roam()
+    myhippo.makeNoise()
+    myhippo.sleep()
+
+    //สร้างแมวของเรา
+    var mycat = cat()
+    mycat.eat()
+    mycat.roam()
+    mycat.makeNoise()
+    mycat.sleep()
 }
+
 open class Animal{
 open  val image = ""
 open  val food = ""
@@ -21,6 +32,31 @@ open  val habitat = ""
         println("The Animal is sleeping")
     }
 }
+
+
 class Hippo : Animal() {
     //สร้างคลาส Hippo แต่ยังไม่ได้ทำอะไร
+    override val  image = "hippo.jpg"
+    override val  food = "grass"
+    override val  habitat = "water"
+    override fun makeNoise() {
+        println("Grunt! Grunt!")
+    }
+
+    override fun eat() {
+        println("The Hippo is eating $food")
+    }
 }
+
+
+class cat : Animal () {
+    override val  image = "cat.jpg"
+    override val  food = "rat"
+    override val  habitat = "house"
+    override fun makeNoise() {
+        println("meow! Meow!")
+    }
+    override fun eat() {
+        println("The cat is eating $food")
+    }
+    }
